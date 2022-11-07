b0c0afc8944f742d5d534933536d51078c3f8f56
Oliver-Schousboe <95352080+Oliver-Schousboe@users.noreply.github.com>
2022-11-07 11:33:29 +0100

    Added event source in character

--- CharacterAppearanceHistory/CharacterAppearanceHistoryApp/Character.cs
+++ CharacterAppearanceHistory/CharacterAppearanceHistoryApp/Character.cs
@@ -1,20 +1,33 @@
 namespace CharacterAppearanceHistoryApp
 {
+
+    public class CharacterChangedEventArgs : EventArgs
+    {
+        ///Data to be passed in event
+
+    }
     // The Originator holds some important state that may change over time. It
     // also defines a method for saving the state inside a memento and another
     // method for restoring the state from it.
-    class Character
+    public class Character
     {
         // Define character states
         private string _color;
         private string _size;
         private string _strength;
+
+        public EventHandler<CharacterChangedEventArgs> CharacterChanged;
         public Character(string newColor, string newSize = "medium", string newStrength = "average")
         {
             _color = newColor;
             _size = newSize;
             _strength = newStrength;
             Console.WriteLine($"Character: I am a {_color}, {_size}, {_strength} character.");
+        }
+
+        public void OnCharacterChanged(CharacterChangedEventArgs e)
+        {
+            CharacterChanged?.Invoke(this, e);
         }
 
         // The Originator's business logic may affect its internal state.
@@ -26,24 +39,30 @@ namespace CharacterAppearanceHistoryApp
             _color = newColor;
             _size = newSize;
             _strength = newStrength;
+            OnCharacterChanged(new CharacterChangedEventArgs());
+
         }
 
         public void ChangeColor(string newColor)
         {
             Console.WriteLine($"Character: I am changing my color to {newColor}.");
             _color = newColor;
+            OnCharacterChanged(new CharacterChangedEventArgs());
+            
         }
 
         public void ChangeSize(string newSize)
         {
             Console.WriteLine($"Character: I am changing my size to {newSize}.");
             _size = newSize;
+            OnCharacterChanged(new CharacterChangedEventArgs());
         }
 
         public void ChangeStrength(string newStrength)
         {
             Console.WriteLine($"Character: I am changing my strength to {newStrength}.");
             _strength = newStrength;
+            OnCharacterChanged(new CharacterChangedEventArgs());
         }
 
         // Saves the current state inside a memento.
@@ -69,5 +88,7 @@ namespace CharacterAppearanceHistoryApp
             _strength = memento.GetStrength();
             Console.WriteLine($", my strength is now changed to: {_strength}");
         }
+
+
     }
 }
